65a7d7fbe6507f7a2388e46860c2ad78f70483ea
Pablo Ronaldo Guevara <Ronaldo@DESKTOP-919BAJ2>
2021-04-30 14:03:07 -0600

    #2->Creacion de clase computadoras

--- src/lab2p2_pabloguevara/Lab2P2_PabloGuevara.java
+++ src/lab2p2_pabloguevara/Lab2P2_PabloGuevara.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2p2_pabloguevara;
 
-/**
- *
- * @author Ronaldo
- */
 public class Lab2P2_PabloGuevara {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
---
+++ src/lab2p2_pabloguevara/computadoras.java
@@ -0,0 +1,107 @@
+package lab2p2_pabloguevara;
+
+import java.util.Date;
+
+public class computadoras {
+    private String modelo;
+    private long NumSerie;
+    private float CapHDD;
+    private float tamPantalla;
+    private String tecladoNum;
+    private String TipoProcesador;
+    private String tarjetaGrafica;
+    private String sistemaOperativo;
+    private Date tiempoFabricacion;
+    private float capBateria;
+    private float duracionBateria;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public long getNumSerie() {
+        return NumSerie;
+    }
+
+    public void setNumSerie(long NumSerie) {
+        this.NumSerie = NumSerie;
+    }
+
+    public float getCapHDD() {
+        return CapHDD;
+    }
+
+    public void setCapHDD(float CapHDD) {
+        this.CapHDD = CapHDD;
+    }
+
+    public float getTamPantalla() {
+        return tamPantalla;
+    }
+
+    public void setTamPantalla(float tamPantalla) {
+        this.tamPantalla = tamPantalla;
+    }
+
+    public String getTecladoNum() {
+        return tecladoNum;
+    }
+
+    public void setTecladoNum(String tecladoNum) {
+        this.tecladoNum = tecladoNum;
+    }
+
+    public String getTipoProcesador() {
+        return TipoProcesador;
+    }
+
+    public void setTipoProcesador(String TipoProcesador) {
+        this.TipoProcesador = TipoProcesador;
+    }
+
+    public String getTarjetaGrafica() {
+        return tarjetaGrafica;
+    }
+
+    public void setTarjetaGrafica(String tarjetaGrafica) {
+        this.tarjetaGrafica = tarjetaGrafica;
+    }
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    public Date getTiempoFabricacion() {
+        return tiempoFabricacion;
+    }
+
+    public void setTiempoFabricacion(Date tiempoFabricacion) {
+        this.tiempoFabricacion = tiempoFabricacion;
+    }
+
+    public float getCapBateria() {
+        return capBateria;
+    }
+
+    public void setCapBateria(float capBateria) {
+        this.capBateria = capBateria;
+    }
+
+    public float getDuracionBateria() {
+        return duracionBateria;
+    }
+
+    public void setDuracionBateria(float duracionBateria) {
+        this.duracionBateria = duracionBateria;
+    }
+    
+    
+}
